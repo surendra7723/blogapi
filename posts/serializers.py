642841b1d989b14be701508e43684961ad1497dc
SurendraCodes <surendra7723@mbmcsit.edu.np>
2024-02-29 18:51:10 +0545

    authentication are handled

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib import get_user_model
 from rest_framework import serializers
 from .models import Post
 class PostSerializer(serializers.ModelSerializer):
@@ -10,3 +11,10 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
             )
         model=Post
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=get_user_model()
+        fields=("id","username",)
+        
+    
